939a185cb5d1e549c7bfd801780ce57a2ea24616
cgapti <cg00482481@techmahindra.com>
2017-01-09 12:26:18 +0530

    Add files via upload
    SOW tracker table and sequence scripts

---
+++ DBScripts/Stn_Dtl_Slno_Seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Wednesday-December-21-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence STN_DTL_SLNO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."STN_DTL_SLNO_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
---
+++ DBScripts/User_Empslno_Seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Wednesday-December-21-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence USER_EMPSLNO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."USER_EMPSLNO_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 101 CACHE 20 NOORDER  NOCYCLE ;
